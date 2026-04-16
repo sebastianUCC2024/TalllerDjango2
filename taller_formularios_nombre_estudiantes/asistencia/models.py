2b7c9298080b5108db82a80cecd870ba4cd14179
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-04-16 09:11:17 -0500

    Correccion de migraciones

--- taller_formularios_nombre_estudiantes/asistencia/models.py
+++ taller_formularios_nombre_estudiantes/asistencia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Asistencia(models.Model):
+    nombreCompleto = models.CharField(max_length=150)
+    documentoIdentidad = models.CharField(max_length=50)
+    correoElectronico = models.EmailField()
+    fechaAsistencia = models.DateField()
+    horaIngreso = models.TimeField()
+    horaSalida = models.TimeField()
+    presente = models.BooleanField(default=False)
+    observaciones = models.CharField(max_length=1000, blank=True)
+
+    def _str_(self):
+        return f"{self.nombreCompleto} - {self.documentoIdentidad} - {self.correoElectronico} - {self.fechaAsistencia} - {self.horaIngreso} - {self.horaSalida} - {'Presente' if self.presente else 'Ausente'} - {self.observaciones}"
