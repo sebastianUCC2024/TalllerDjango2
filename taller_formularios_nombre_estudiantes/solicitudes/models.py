25ec8838cf3b2cc39760d1b4e411f93e33f73b4a
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-04-16 08:39:02 -0500

    Se realizo las migraciones

--- taller_formularios_nombre_estudiantes/solicitudes/models.py
+++ taller_formularios_nombre_estudiantes/solicitudes/models.py
@@ -9,7 +9,7 @@ class Solicitudes(models.Model):
     asusto = models.CharField(max_length=100)
     descripcionDetallada = models.CharField(max_length=200)
     fechaSolicitud = models.DateTimeField()
-    archivoAdjunto = models.FieldFile(upload_to='archivos/',null=True,blank=True)
+    archivoAdjunto = models.FileField(upload_to='archivos/',null=True,blank=True)
     
     def __str__(self):
         return f"{self.nombreSolicitante} - {self.documentoIdentidad} - {self.correoElectronico} - {self.telefonoContacto} - {self.asusto} - {self.descripcionDetallada} - {self.fechaSolicitud} - {self.archivoAdjunto}"
